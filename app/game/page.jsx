eba953497f07c13cddfa9e7f8faf6990d53946af
Charly Chacon <charlyjchaconc@gmail.com>
2022-12-30 00:26:21 -0500

    initialized game

--- app/game/page.jsx
+++ app/game/page.jsx
@@ -2,14 +2,25 @@
 
 export default function Game () {
     const shoot = () => {
-        
+        let starship = document.querySelector(".starship");
+        // let starshipX = starship.offsetLeft;
+        let midStarship = starship.offsetWidth / 2;
+        let scenario = document.querySelector(".scenario")
+
+        console.log(midStarship)
         let fire = document.createElement("span");
         
         fire.classList.add("fireshoot")
-        fire.style.display = "block"
-        let space = document.querySelector(".scenario");
-        space.appendChild(fire);
-        console.log(space)
+        fire.style.display = "block";
+        fire.style.position = "absolute"
+        fire.style.left = `${midStarship}px`
+        fire.style.top = '-10px'
+        fire.style.backgroundColor = "yellow"
+        // let space = document.querySelector(".scenario");
+        starship.appendChild(fire);
+        // const shooting = setInterval(() => {
+        //     const 
+        // }, 10);
     }
     return <>
     <div className="game">
